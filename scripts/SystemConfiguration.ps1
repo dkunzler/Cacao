b5944af03b67dad8a2caf5b346bb3e6c5145e21c
David Kunzler <dk@devland.de>
2021-10-24 23:10:54 +0200

    set script policy and enable administrative shares

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -2,3 +2,9 @@
 
 #--- Enable developer mode on the system ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
+
+# Change ExecutionPolicy
+Set-ExecutionPolicy RemoteSigned
+
+#--- Enable administrative shares ---
+Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 1
