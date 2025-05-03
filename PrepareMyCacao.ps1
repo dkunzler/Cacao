bb502fe2e5f0ad2b3a6ec4edabf3b9486a5f3afb
David Kunzler <dk@devland.de>
2025-05-03 19:51:42 +0200

    Aktualisieren von PrepareMyCacao.ps1

--- PrepareMyCacao.ps1
+++ PrepareMyCacao.ps1
@@ -24,7 +24,7 @@ function executeScript {
 #--- Setting up Windows ---
 executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
-executeScript "RemoveDefaultApps.ps1";
+# executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
 executeScript "Browsers.ps1";
 executeScript "WindowsTools.ps1";
