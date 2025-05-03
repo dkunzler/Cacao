bb3824fbd05593669e106122bf1f033d4cdb07ed
David Kunzler <dk@devland.de>
2025-05-03 16:52:26 +0200

    use correct executionpolicy command

--- scripts/SystemConfiguration.ps1
+++ scripts/SystemConfiguration.ps1
@@ -4,7 +4,7 @@
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\AppModelUnlock -Name AllowDevelopmentWithoutDevLicense -Value 1
 
 # Change ExecutionPolicy
-Update-ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy RemoteSigned
 
 #--- Enable administrative shares ---
 Set-ItemProperty -Path HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 1
